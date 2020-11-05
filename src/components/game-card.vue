6cc0781196c0c61cd5e0a6f478aa5ea1156162ae
m-alghobary <m.alghobary90@gmail.com>
2020-11-05 10:18:47 +0300

    add add flip animation to cards

--- src/components/game-card.vue
+++ src/components/game-card.vue
@@ -1,12 +1,13 @@
 <template>
-	<div class="w-32 h-32 relative rounded cursor-pointer" @click="flip">
-		<div
-			v-if="!isFlipped"
-			class="bg-teal-300 absolute inset-0 w-full h-full rounded shadow-md flex items-center justify-center text-3xl font-semibold text-pink-500"
-		>
-			{{ type }}
+	<div class="w-32 h-32 relative rounded cursor-pointer card" @click="flip">
+		<div class="content absolute inset-0 w-full h-full" :class="{ flip: !isFlipped }">
+			<div
+				class="face bg-teal-300 absolute inset-0 w-full h-full rounded shadow-md flex items-center justify-center text-3xl font-semibold text-pink-500"
+			>
+				{{ type }}
+			</div>
+			<div class="back bg-teal-400 absolute inset-0 w-full h-full rounded shadow-md"></div>
 		</div>
-		<div v-else class="bg-teal-400 absolute inset-0 w-full h-full rounded shadow-md"></div>
 	</div>
 </template>
 
@@ -35,4 +36,27 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.card {
+	perspective: 500px;
+}
+
+.content {
+	transition: transform 1s;
+	transform-style: preserve-3d;
+}
+
+.flip {
+	transform: rotateY(180deg);
+	transition: transform 0.5s;
+}
+
+.face,
+.back {
+	backface-visibility: hidden;
+}
+
+.face {
+	transform: rotateY(180deg);
+}
+</style>
