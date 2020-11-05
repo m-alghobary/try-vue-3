9617ff6742db69e40dedac8032b700b6c931152f
m-alghobary <m.alghobary90@gmail.com>
2020-11-05 09:26:06 +0300

    setup the game board

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-	<h1>Peek-A-Vue</h1>
+	<h1 class="text-center mt-6 text-2xl">Peek-A-Vue</h1>
+	<div class="max-w-2xl mt-5 mx-auto p-4 grid grid-cols-4 grid-rows-4 gap-4">
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+		<div class="bg-gray-300 w-32 h-32"></div>
+	</div>
 </template>
 
 <script>
