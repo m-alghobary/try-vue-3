a5613d937b2cb627d40bbb7e2af5fc7a052aecbd
m-alghobary <m.alghobary90@gmail.com>
2020-11-05 09:14:55 +0300

    delete unwanted code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+	<h1>Peek-A-Vue</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	components: {},
+};
 </script>
