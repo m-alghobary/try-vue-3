8392c48cf6393a3dd905e4b41f5efc1943da8fc3
m-alghobary <m.alghobary90@gmail.com>
2020-11-05 09:53:20 +0300

    add separet card component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
 	<h1 class="text-center mt-6 text-2xl">Peek-A-Vue</h1>
 	<div class="max-w-2xl mt-5 mx-auto p-4 grid grid-cols-4 grid-rows-4 gap-4">
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
-		<div class="bg-gray-300 w-32 h-32"></div>
+		<game-card v-for="(card, index) in cards" :key="index" :type="card"></game-card>
 	</div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import GameCard from './components/game-card.vue';
+
 export default {
 	name: 'App',
-	components: {},
+	components: {
+		GameCard,
+	},
+	setup() {
+		const cards = ref([]);
+
+		for (let i = 0; i < 16; i++) {
+			cards.value.push(i);
+		}
+
+		return {
+			cards,
+		};
+	},
 };
 </script>
---
+++ src/components/game-card.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="bg-gray-300 w-32 h-32">{{ type }}</div>
+</template>
+
+<script>
+export default {
+	props: {
+		type: {
+			type: Number,
+			required: true,
+		},
+	},
+	setup(props) {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
