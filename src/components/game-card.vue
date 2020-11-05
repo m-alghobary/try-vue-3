8f229029fbf1dcd907772d886985ad04d284043b
m-alghobary <m.alghobary90@gmail.com>
2020-11-05 10:05:43 +0300

    add flip ability to cards

--- src/components/game-card.vue
+++ src/components/game-card.vue
@@ -1,8 +1,18 @@
 <template>
-	<div class="bg-gray-300 w-32 h-32">{{ type }}</div>
+	<div class="w-32 h-32 relative rounded cursor-pointer" @click="flip">
+		<div
+			v-if="!isFlipped"
+			class="bg-teal-300 absolute inset-0 w-full h-full rounded shadow-md flex items-center justify-center text-3xl font-semibold text-pink-500"
+		>
+			{{ type }}
+		</div>
+		<div v-else class="bg-teal-400 absolute inset-0 w-full h-full rounded shadow-md"></div>
+	</div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
 	props: {
 		type: {
@@ -11,7 +21,16 @@ export default {
 		},
 	},
 	setup(props) {
-		return {};
+		const isFlipped = ref(true);
+
+		function flip() {
+			isFlipped.value = !isFlipped.value;
+		}
+
+		return {
+			isFlipped,
+			flip,
+		};
 	},
 };
 </script>
